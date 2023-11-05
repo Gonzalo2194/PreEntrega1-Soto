6ce2e0c8b917989385d37fd8e3163c3343fa9585
Gonzalo2194 <gonzalosoto.2194@gmail.com>
2023-11-05 19:39:06 +0100

    se crea Components/ NavBar

---
+++ src/Components/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Bulma Logo" />
+                </a>
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a className="navbar-item">Inicio</a>
+                    <a className="navbar-item">Categorias</a>
+                    <a className="navbar-item">Ayuda</a>
+                </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-primary">
+                                <strong>Regístrate</strong>
+                            </a>
+                            <a className="button is-light">
+                                Inicia sesión
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
