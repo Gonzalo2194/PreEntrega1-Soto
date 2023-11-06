8d98df554e22f708c4f4d25bda1247c4915bf4e6
Gonzalo2194 <gonzalosoto.2194@gmail.com>
2023-11-06 21:17:42 +0100

    se crea y ajusta logo compraclic

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,11 +1,17 @@
 import CartWidget from "../CartWidget/cart";
+import logo from'./asset/logo.jpg';
+
 
 const NavBar = () => {
+    const logoStyle = {
+        width: '90px',  
+        height: '90px',
+    }
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Bulma Logo" />
+                <a className="navbar-item" href="#">
+                    <img src={logo} alt="compraclic-logo"style={logoStyle}/>
                 </a>
                 <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
@@ -19,10 +25,9 @@ const NavBar = () => {
                     <div class="navbar-item has-dropdown is-hoverable">
                     <a class="navbar-link">Categorias</a>
                         <div class="navbar-dropdown">
-                            <a class="navbar-item">Categ 3</a>
-                            <a class="navbar-item">Categ 2</a>
-                            <a class="navbar-item">Categ 2</a>
-                            <a class="navbar-item">Categ 4</a>
+                            <a class="navbar-item">Electrónica y Tecnología</a>
+                            <a class="navbar-item">Hogar y Decoración</a>
+                            <a class="navbar-item">Ropa y Accesorios</a>
                         </div>
                 </div>
                 <a className="navbar-item">Ayuda</a>
