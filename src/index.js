ee24316382cfbffd5f801b5a54e8b3d1659bdb8f
Gonzalo2194 <gonzalosoto.2194@gmail.com>
2023-11-06 21:34:23 +0100

    se agrega itemlistcontainer, se modifica logo compraclic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import 	'bulma/css/bulma.min.css';
 import NavBar from './Components/NavBar/navbar.js';
-
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer.js';
 
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
@@ -11,6 +11,7 @@ function App() {
     return (
         <div>
             <NavBar/>
+            <ItemListContainer greeting={"Bienvenido a compraclic"}/>
         </div>
     );
 }
